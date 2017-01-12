b148c7edafe5eaee8a11c4d6c16e007946d6a8cf
Zhou Wang <zhouwang@microsoft.com>
2017-01-12 11:35:38 +0100

    rename CNTKLibraryManagedExamples to CNTKLibraryCSExamples

---
+++ Tests/EndToEndTests/EvalClientTests/CNTKLibraryManagedExamplesTest/program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CNTK;
+
+namespace CNTKLibraryManagedExamples
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("======== Evaluate model using C# ========");
+
+            CNTKLibraryManagedExamples.EvaluationSingleImage(DeviceDescriptor.CPUDevice);
+            CNTKLibraryManagedExamples.EvaluationBatchOfImages(DeviceDescriptor.CPUDevice);
+            //TODO: Add examples with OneHot.
+            //EvaluationSingleSequenceUsingOneHot(DeviceDescriptor.CPUDevice);
+            //EvaluationBatchOfSequencesUsingOneHot(DeviceDescriptor.CPUDevice);
+
+            // TODO: using GPU.
+            //EvaluationSingleImage(DeviceDescriptor.GPUDevice(0));
+            //EvaluationBatchOfImages(DeviceDescriptor.GPUDevice(0));
+
+            Console.WriteLine("======== Evaluation completes. ========");
+        }
+    }
+}

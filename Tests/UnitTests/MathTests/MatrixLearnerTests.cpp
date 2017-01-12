5147dae80d6b5c52cf08195354dcc1c56f13a29c
KeDengMS <kedeng@microsoft.com>
2017-01-11 23:57:50 -0800

    Update baseline and clean hard tabs

--- Tests/UnitTests/MathTests/MatrixLearnerTests.cpp
+++ Tests/UnitTests/MathTests/MatrixLearnerTests.cpp
@@ -18,45 +18,45 @@ BOOST_AUTO_TEST_SUITE(MatrixLearnerTests)
 
 BOOST_FIXTURE_TEST_CASE(FSAdagradTest, RandomSeedFixture)
 {
-	// compare dense and sparse result
-	const size_t dim1 = 256;
-	const size_t dim2 = 128;
-	const size_t dim3 = 2048;
+    // compare dense and sparse result
+    const size_t dim1 = 256;
+    const size_t dim2 = 128;
+    const size_t dim3 = 2048;
 
-	// smoothed gradient
-	SingleMatrix matSG = SingleMatrix::RandomGaussian(dim1, dim2, c_deviceIdZero, -1.0f, 1.0f, IncrementCounter());
-	SingleMatrix matSGsparse(matSG.DeepClone());
+    // smoothed gradient
+    SingleMatrix matSG = SingleMatrix::RandomGaussian(dim1, dim2, c_deviceIdZero, -1.0f, 1.0f, IncrementCounter());
+    SingleMatrix matSGsparse(matSG.DeepClone());
 
-	// model
-	SingleMatrix matM  = SingleMatrix::RandomGaussian(dim1, dim2, c_deviceIdZero, -1.0f, 1.0f, IncrementCounter());
-	SingleMatrix matMsparse(matM.DeepClone());
+    // model
+    SingleMatrix matM  = SingleMatrix::RandomGaussian(dim1, dim2, c_deviceIdZero, -1.0f, 1.0f, IncrementCounter());
+    SingleMatrix matMsparse(matM.DeepClone());
 
-	// generates gradient
-	SingleMatrix matG1(c_deviceIdZero);
-	matG1.AssignTruncateBottomOf(Matrix<float>::RandomUniform(dim2, dim3, c_deviceIdZero, -300.0f, 0.1f, IncrementCounter()), 0);
+    // generates gradient
+    SingleMatrix matG1(c_deviceIdZero);
+    matG1.AssignTruncateBottomOf(Matrix<float>::RandomUniform(dim2, dim3, c_deviceIdZero, -300.0f, 0.1f, IncrementCounter()), 0);
 
-	SingleMatrix matG1sparseCSC(matG1.DeepClone());
-	matG1sparseCSC.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseCSC, true);
+    SingleMatrix matG1sparseCSC(matG1.DeepClone());
+    matG1sparseCSC.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseCSC, true);
 
-	SingleMatrix matG2 = SingleMatrix::RandomGaussian(dim1, dim3, c_deviceIdZero, -1.0f, 1.0f, IncrementCounter());
+    SingleMatrix matG2 = SingleMatrix::RandomGaussian(dim1, dim3, c_deviceIdZero, -1.0f, 1.0f, IncrementCounter());
 
-	SingleMatrix matG(c_deviceIdZero);
-	matG.SetValue(0.0);
-	SingleMatrix::MultiplyAndAdd(matG2, false, matG1, true, matG);
+    SingleMatrix matG(c_deviceIdZero);
+    matG.SetValue(0.0);
+    SingleMatrix::MultiplyAndAdd(matG2, false, matG1, true, matG);
 
-	SingleMatrix matGsparseBSC(c_deviceIdZero);
-	matGsparseBSC.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseBlockCol, false);
-	SingleMatrix::MultiplyAndAdd(matG2, false, matG1sparseCSC, true, matGsparseBSC);
+    SingleMatrix matGsparseBSC(c_deviceIdZero);
+    matGsparseBSC.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseBlockCol, false);
+    SingleMatrix::MultiplyAndAdd(matG2, false, matG1sparseCSC, true, matGsparseBSC);
 
-	// run learner
-	double smoothedCount = 1000;
-	matSG.FSAdagradUpdate(dim2, matG, matM, smoothedCount, 0.0001, 1.0, 0.9, 0.9);
+    // run learner
+    double smoothedCount = 1000;
+    matSG.FSAdagradUpdate(dim2, matG, matM, smoothedCount, 0.0001, 1.0, 0.9, 0.9);
 
-	smoothedCount = 1000;
-	matSGsparse.FSAdagradUpdate(dim2, matGsparseBSC, matMsparse, smoothedCount, 0.0001, 1.0, 0.9, 0.9);
+    smoothedCount = 1000;
+    matSGsparse.FSAdagradUpdate(dim2, matGsparseBSC, matMsparse, smoothedCount, 0.0001, 1.0, 0.9, 0.9);
 
-	BOOST_CHECK(matSG.IsEqualTo(matSGsparse, c_epsilonFloatE5));
-	BOOST_CHECK(matM.IsEqualTo(matMsparse, c_epsilonFloatE5));
+    BOOST_CHECK(matSG.IsEqualTo(matSGsparse, c_epsilonFloatE5));
+    BOOST_CHECK(matM.IsEqualTo(matMsparse, c_epsilonFloatE5));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
